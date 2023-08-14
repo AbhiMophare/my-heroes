860bcdd83ad6b9db53c1cd2de21d9d1cc4bcb748
AbhiMophare <77708148+AbhiMophare@users.noreply.github.com>
2023-08-14 13:30:37 +0530

    2 files modified

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,13 +7,16 @@ import { Hero } from '../hero';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnChanges {
   @Input() selectedHero!: Hero;
   constructor() {
     // console.log(this.selectedHero);
   }
 
   ngOnInit() {
+    // console.log(this.selectedHero);
+  }
+  ngOnChanges() {
     console.log(this.selectedHero);
   }
 }
